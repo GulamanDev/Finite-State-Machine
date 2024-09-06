e9926881012f8d94b2bbc9835517150ebeac46c4
siararisa-iac <126797704+siararisa-iac@users.noreply.github.com>
2024-09-07 07:25:53 +0800

    Fixed compiler error

--- Assets/Scripts/AdvancedFSM/States/ChaseState.cs
+++ Assets/Scripts/AdvancedFSM/States/ChaseState.cs
@@ -4,15 +4,10 @@ using UnityEngine;
 
 public class ChaseState : FSMState
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public override void RunState(Transform player, Transform agent){
 
-    // Update is called once per frame
-    void Update()
-    {
+    }
+    public override void CheckTransitionRules(Transform player, Transform agent){
         
     }
 }
